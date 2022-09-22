3b81fb0d8d1e5e9b6951422859312deffb235e29
guillsil <111441275+guillsil@users.noreply.github.com>
2022-09-22 12:04:26 -0300

    update

---
+++ JS/Clase 13 - JavaScript 1/app.js
@@ -0,0 +1,31 @@
+//Comentario en linea
+
+/*
+Comentario en bloque
+*/
+
+console.log("HOLA MUNDO DESDE ARCHIVO EXTERNO!")
+
+//Variables se inicializan con let, var, const
+
+let numero = newNumber(prompt("Ingrese el numero 1:"));    //Dato numero no tiene comillas
+let numero2 = newNumber(prompt("Ingrese el numero 2:"));
+
+let nombre = "Arturo";   //string tiene comillas
+
+console.log(numero);
+console.log(nombre);
+console.log(numero+numero2)
+
+let numero3;
+console.log(numero3);
+console.log(typeof numero3)
+
+numero3 = 25;
+
+console.log(numero3);
+console.log(typeof numero3)
+
+numero3= 20;
+
+console.log(numero3);
